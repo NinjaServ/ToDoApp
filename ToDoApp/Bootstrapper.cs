99b0fb1fb28211516a3b056df024bc513a86bf7b
Seth Urbach <urbachs@gmail.com>
2017-09-02 14:32:57 -0700

    Add project files.

---
+++ ToDoApp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using ToDoApp.Views;
+using System.Windows;
+
+namespace ToDoApp
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
